1fa5231c573455b06a896f70f14bb384e329188b
TasnadiAttila <tasnadi.attila21@gmail.com>
2022-02-23 04:35:32 -0800

    feladat 3

--- Drill3/drill.cpp
+++ Drill3/drill.cpp
@@ -16,13 +16,16 @@ class D1 : public B1{
         };
 };
 int main(){
-    B1 b;
-    b.vf();
-    b.f();
-    D1 d;
-    d.vf();
-    
+    B1 b_object;
+    b_object.vf();
+    b_object.f();
 
+    D1 d_object;
+    d_object.vf();
+    
+    B1& b_ref_obj = d_object;
+    b_ref_obj.vf();
+    b_ref_obj.f();
 
 
     return 0;
