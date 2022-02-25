352253677520193ce2923d1239e9073340d63bfb
TasnadiAttila <tasnadi.attila21@gmail.com>
2022-02-25 11:40:51 +0100

    6-7

--- Drill3/drill.cpp
+++ Drill3/drill.cpp
@@ -27,18 +27,54 @@ class D1 : public B1{
             cout << "D1::pvf()" << endl;
         }
 };
+
+//6
+class D2 : public D1{
+    public:
+        void pvf(){
+            cout << "D2:pvf()" << endl;
+        }
+};
+
+//7
+class B2{
+    public:
+        virtual void pvf() = 0;
+            
+};
+class D21 : public B2{
+    public:
+        string overrideTEXT;
+        void pvf() override { 
+            cout << overrideTEXT << endl;
+        }
+    
+};
+class D22 : public B2 {
+    public:
+        int dataMember;
+        void pvf(){
+            cout << dataMember << endl;
+        }
+        void f(B2& R){
+            R.pvf();
+        }
+};
+
 int main(){
     //1
     B1 b_object;
     b_object.vf();
     b_object.f();
 
+    cout << " " << endl;
     //2
     D1 d_object;
     d_object.vf();
     //4
     d_object.f();
     
+    cout << " " << endl;
     //3
     B1& b_ref_obj = d_object;
     b_ref_obj.vf();
@@ -49,6 +85,26 @@ int main(){
     b_object.pvf();//its from B1
     d_object.pvf();//The class D has its own pvf
     b_ref_obj.pvf();//B ref is refers to D
+
+    cout << " " << endl;
+    //6 
+    //ahonnan származik azokat írja kivéve a pvf()-nél mert abból van sajátja
+    D2 d2_object;
+    d2_object.f();
+    d2_object.vf();
+    d2_object.pvf();
+
+    cout << " " << endl;
+    //7
+    D21 d21_obj;
+    d21_obj.overrideTEXT = "Over ride text";
+    D22 d22_obj;
+    d22_obj.dataMember = 8;
+
+    d22_obj.f(d21_obj);
+    d22_obj.f(d22_obj);
+
+
 
 
 
