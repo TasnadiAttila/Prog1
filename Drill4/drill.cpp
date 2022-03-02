e0bbaacdab619d1e3b42a330c58b4ec00dca8238
TasnadiAttila <tasnadi.attila21@gmail.com>
2022-03-02 13:41:25 +0100

    nemjo cos

--- Drill4/drill.cpp
+++ Drill4/drill.cpp
@@ -1,8 +1,9 @@
 #include "Simple_window.h"
 #include "Graph.h"
-//1,4
+//1,4,6
 double one(double x) { return 1;};
 double slope(double x) {return x/2;};
+double square(double x) {return x*x;};
 
 int main(){
     using namespace Graph_lib;
@@ -34,15 +35,16 @@ int main(){
     //5
     Function slopy{slope,r_min,r_max,origo,n_points,xscale,yscale};
     Text text_for_slopy{Point{100,385},"x/2"};
-
-
+    //7
+    Function cosF{cos,r_min,r_max,n_points,xscale,yscale};
+    
 
     win.attach(x);
     win.attach(y);
     win.attach(f);
     win.attach(slopy);
     win.attach(text_for_slopy);
-
+    win.attach(cosinus);
 
 
     win.wait_for_button();
