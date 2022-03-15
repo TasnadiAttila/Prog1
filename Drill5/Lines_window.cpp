b2c33a4008eb9b3e1141f4ff066d876ba88060c9
TasnadiAttila <tasnadi.attila21@gmail.com>
2022-03-15 12:28:35 +0100

    Line Changer

--- Drill5/Lines_window.cpp
+++ Drill5/Lines_window.cpp
@@ -5,7 +5,8 @@ Lines_window::Lines_window(Point xy, int w, int h, const string& title)
     color_menu{Point{x_max()-70,40},70,20,Menu::vertical,"Color"},
     menu_button{Point{x_max()-80,40},80,20,"color menu",cb_menu},
     //
-    
+    style_menu{Point{x_max()-70,110},80,20,Menu::vertical,"Style"},
+    style_button{Point{x_max()-80,110},80,20,"Style menu",cb_style_menu},
 
     next_button{Point{x_max()-300,0}, 150, 40, "Next point",
         [](Address, Address pw) {reference_to<Lines_window>(pw).next();}},
@@ -30,7 +31,15 @@ Lines_window::Lines_window(Point xy, int w, int h, const string& title)
     color_menu.hide();
     attach(menu_button);
 
+    style_menu.attach(new Button{Point{0,0},0,0,"Solid",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::solid);}});
+    style_menu.attach(new Button{Point{0,0},0,0,"Dash",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::dash);}});
+    style_menu.attach(new Button{Point{0,0},0,0,"Dot",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::dot);}});
+    style_menu.attach(new Button{Point{0,0},0,0,"Dashdot",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::dashdot);}});
+    style_menu.attach(new Button{Point{0,0},0,0,"Dashdotdot",[](Address,Address pw) {reference_to<Lines_window>(pw).change_style(Line_style::dashdotdot);}});
 
+    attach(style_menu);
+    style_menu.hide();
+    attach(style_button);
     
 
 
@@ -70,4 +79,7 @@ void Lines_window::cb_blue(Address,Address pw){
 }
 void Lines_window::cb_menu(Address,Address pw){
     reference_to<Lines_window>(pw).menu_pressed();
+}
+void Lines_window::cb_style_menu(Address,Address pw){
+    reference_to<Lines_window>(pw).style_menu_pressed();
 }
--- Drill5/Lines_window.h
+++ Drill5/Lines_window.h
@@ -9,13 +9,14 @@ struct Lines_window : Graph_lib::Window {
     Menu color_menu;
 	Button menu_button;
 	//
-	
+	Menu style_menu;
+	Button style_button;
 	
 
 	void change(Color c){lines.set_color(c);}
     void hide_menu(){color_menu.hide(); menu_button.show();}
 	//
-	
+	void hide_style_menu(){style_menu.hide(); style_button.show();}
 
 	
     void red_pressed(){change(Color::red);redraw();}
@@ -23,14 +24,15 @@ struct Lines_window : Graph_lib::Window {
     void black_pressed(){change(Color::black);redraw();}
 	void menu_pressed(){menu_button.hide();color_menu.show();}
 	//
-	
+	void change_style(Line_style ls){lines.set_style(ls); hide_style_menu(); redraw();}
+	void style_menu_pressed(){style_button.hide();style_menu.show();}
     
 	static void cb_red(Address,Address);
 	static void cb_blue(Address,Address);
     static void cb_black(Address,Address);
 	static void cb_menu(Address,Address);
 	//
-	
+	static void cb_style_menu(Address,Address);
 	
 
     bool wait_for_button();
