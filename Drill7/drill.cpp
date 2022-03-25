160bb4803bd580e695dd8129db98896f8a0ea335
TasnadiAttila <tasnadi.attila21@gmail.com>
2022-03-25 16:49:11 +0100

    Drill 7 complete

--- Drill7/drill.cpp
+++ Drill7/drill.cpp
@@ -1,14 +1,26 @@
 #include "../std_lib_facilities.h"
+//812
+template<typename Iter1, typename Iter2>
+Iter2 MyCopy(Iter1 f1, Iter1 e1, Iter2 f2){
+    while (f1 != e1)
+	{
+		*f2 = *f1;
+		++f1;
+		++f2;
+	}
+
+	return f2;
+}
 
 //iterator- elemre mutató pointer 
 int main()
 {
-    int MyArray[10] = {0,1,2,3,4,5,6,7,8,9};
+    array <int,10> MyArray = {0,1,2,3,4,5,6,7,8,9};
     vector<int> MyVector = {0,1,2,3,4,5,6,7,8,9};
     list<int> MyList = {0,1,2,3,4,5,6,7,8,9};
 
     //copy array to array and increase value by 2
-    int MyArray2[10];
+    array <int,10> MyArray2;
     for(int i=0; i< 10;++i){
         MyArray2[i] = MyArray[i]+2;
     }
@@ -25,6 +37,47 @@ int main()
          i=i+5;
     }
  
+    
+
+    MyCopy(MyArray.begin(),MyArray.end(),MyVector2.begin());
+    MyCopy(MyList.begin(),MyList.end(),MyArray2.begin());
+
+    auto position = find(MyVector2.begin(),MyVector2.end(),3);//most egy memory address
+    if(position == MyVector2.end())
+    {
+        cout << "Nincs benne" << endl;
+    }
+    else
+    {
+        for(int i = 0;i<MyVector2.size();++i)
+        {
+            if(MyVector2[i]== *position){//ha az a memória címet megtalálja akkor ahha
+                cout << "Benne van" << " a " << i << ". helyen" << endl;
+            }
+        }
+    }
+    
+    auto position2 = find(MyList2.begin(),MyList2.end(),27);//most egy memory address
+    if(position2 == MyList2.end())
+    {
+        cout << "Nincs benne" << endl;
+    }
+    else
+    {
+        for(int i = 0;i<MyVector2.size();++i)
+        {
+            if(MyVector2[i]== *position2){//ha az a memória címet megtalálja akkor ahha
+                cout << "Benne van" << " " << i << ". helyen" << endl;
+            }
+        }
+    }
+
+}
+
+    //for(int i = 0; i < 10;++i){
+    //    cout << MyArray[i] << " " << MyVector2[i] << endl;
+   // }
+    
 
 /*
     for(int i = 0; i < 10;++i){
@@ -42,4 +95,3 @@ int main()
     }
 */
     
-}
