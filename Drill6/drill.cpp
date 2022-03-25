cb1ebf6feec0c8881551fc200d92cac0e94ad417
TasnadiAttila <tasnadi.attila21@gmail.com>
2022-03-25 17:46:49 +0100

    Drill 6 Done

--- Drill6/drill.cpp
+++ Drill6/drill.cpp
@@ -23,6 +23,10 @@ template<typename T> T& S<T>::operator=(const T& a){
     val = a;
     return val;
 }
+template<typename T> const T& S<T>::get()const
+{
+    return val;
+}
 template<class T> ostream& operator<<(ostream& os, const vector<T>& a){
     os << "{ ";
     for(int i = 0; i<a.size();++i){
@@ -35,9 +39,49 @@ template<class T> ostream& operator<<(ostream& os, const vector<T>& a){
     os << "}";
     return os;
 }
+//
+template<typename T> void read_val(T& v){
+    cin >> v;
+}
+template<typename T> istream& operator>>(istream& is, S<T>& tt)
+{
+    T v;
+    cin >> v;
+    if(!is)
+    {
+        return is;
+    }
+    tt=v;
+    return is;
+}
 
-
-
+template<class T> istream& operator>>(istream& is, vector<T>& a){
+    char n1;
+    char n2;
+    T temp;
+    vector<T> v_temp;
+    is >> n1;
+    if(!is){
+        return is;
+    }
+    if(n1!='{')
+    {
+        is.clear();
+        error("{ expected! ");
+        return is;
+    }
+    while (cin>>temp>>n2 && n2==',')
+    {
+        v_temp.push_back(temp);
+    }
+    if(n2!='}'){
+        is.clear();
+        error("} expected! ");
+        return is;
+    }
+    v_temp.push_back(temp);
+    a = v_temp;
+}
 
 int main(){
     //Incizialialas
@@ -69,7 +113,35 @@ int main(){
     cout << "sVECTOR " << sVECTOR.get() << endl;
 
     //inicialialas 3
-    sINT = 5;
-    
-    cout << sINT.get() << endl;
+    cout << endl;
+    sINT = 15;
+    sCHAR = '$';
+    sDOUBLE = 107.1;
+    sSTRING = "DEstring";
+    sVECTOR = {11,12,13,14,15};
+
+    cout << "sINT " << sINT.get() << endl;
+    cout << "sCHAR " << sCHAR.get() << endl;
+    cout << "sDOUBLE " << sDOUBLE.get() << endl;    
+    cout << "sSTRING " << sSTRING.get() << endl;
+    cout << "sVECTOR " << sVECTOR.get() << endl;
+
+    cout<<"sINT: ";
+    read_val(sINT);
+    cout<<"sCHAR: ";
+    read_val(sCHAR);  
+    cout<<"sDOUBLE: ";
+    read_val(sDOUBLE);
+    cout<<"sSTRING: ";
+    read_val(sSTRING);
+    cout<<"sVECTOR :";
+    read_val(sVECTOR);
+
+    cout << "sINT " << sINT.get() << endl;
+    cout << "sCHAR " << sCHAR.get() << endl;
+    cout << "sDOUBLE " << sDOUBLE.get() << endl;    
+    cout << "sSTRING " << sSTRING.get() << endl;
+    cout << "sVECTOR " << sVECTOR.get() << endl;
+
+
 }
